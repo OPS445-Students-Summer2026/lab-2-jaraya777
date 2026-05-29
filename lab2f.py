af75ef2f91f216a9a8c79faafd1c2780221d8276
Justin Araya <jaraya1@myseneca.ca>
2026-05-29 04:17:44 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+##############################
+#                            #
+#  Author: Justin Araya      #
+#                            #
+#  Author ID: 110545258      #
+#                            #
+#  Date Created: 2026/05/29  #
+#                            #
+##############################
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
