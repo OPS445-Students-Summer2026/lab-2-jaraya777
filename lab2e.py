e62c7c0022f45e78772c9ee7cc0edae24880b64d
Justin Araya <jaraya1@myseneca.ca>
2026-05-29 04:15:43 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+##############################
+#                            #
+#  Author: Justin Araya      #
+#                            #
+#  Author ID: 110545258      #
+#                            #
+#  Date Created: 2026/05/29  #
+#                            #
+##############################
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
